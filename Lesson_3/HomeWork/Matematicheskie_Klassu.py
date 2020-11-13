8cc2f10bf5290dade9dc22fbca628bee38410983
CjVizer <cj_vizer@ukr.net>
2020-11-13 21:41:29 +0200

    Changed Matematicheskie_Klassu

--- Lesson_3/HomeWork/Matematicheskie_Klassu.py
+++ Lesson_3/HomeWork/Matematicheskie_Klassu.py
@@ -1,5 +1,9 @@
-students_count = int(input('Сколько учеников в первом математическом классе?: '))
-students_count += int(input('Сколько учеников во втором математическом классе?: '))
-students_count += int(input('Сколько учеников в третьем математическом классе?: '))
+students_1 = int(input('Сколько учеников в первом математическом классе?: '))
+students_2 = int(input('Сколько учеников во втором математическом классе?: '))
+students_3 = int(input('Сколько учеников в третьем математическом классе?: '))
 
-print(students_count // 2 + students_count % 2)
+desks = students_1 // 2 + students_1 % 2
+desks += students_2 // 2 + students_2 % 2
+desks += students_3 // 2 + students_3 % 2
+
+print(desks)
