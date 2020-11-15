49a4a3c176e5e7c62b7d0c76841af532f8da09e1
CjVizer <cj_vizer@ukr.net>
2020-11-15 22:31:59 +0200

    HomeWork String_Slices

---
+++ Lesson_4/HomeWork/String_Slices.py
@@ -0,0 +1,11 @@
+s = input('Введите строку: ')
+
+print('a.', s[2])
+print('b.', s[-2])
+print('c.', s[:5])
+print('d.', s[:len(s)-2])
+print('e.', s[::2])
+print('f.', s[1::2])
+print('g.', s[::-1])
+print('h.', s[-1::-2])
+print('i.', len(s))
