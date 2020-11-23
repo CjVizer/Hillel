b4f4c3a0f58e88be74c55c2d0c21963caa0ef1d2
CjVizer <cj_vizer@ukr.net>
2020-11-23 17:39:51 +0200

    HomeWork_Lesson_6_Insert_Numbers

--- Lesson_6/HomeWork/Insert_Number.py
+++ Lesson_6/HomeWork/Insert_Number.py
@@ -1,15 +1,14 @@
 import random
 
-k = -2
+k = 7
 C = 55
 
-lst = [random.randint(1, 10) for _ in range(20)]
+lst = [random.randint(1, 10) for _ in range(10)]
 print(lst)
-# lst.append(0)
-# print(lst)
+lst.append(0)
 
-# for i in range(len(lst)-1, k, -1):
-#     lst[i] = lst[i-1]
-# lst[k] = C
-lst.insert(-1, 33)
+for i in range(len(lst) - 1, k, -1):
+    lst[i] = lst[i - 1]
+lst[k] = C
+
 print(lst)
