9a0230552a493d1e6e46bd55f75e496b9c88e55d
CjVizer <cj_vizer@ukr.net>
2020-11-25 08:21:30 +0200

    HomeWork_Lesson_6_Sets_1_Changed

--- Lesson_6/HomeWork/Sets_1.py
+++ Lesson_6/HomeWork/Sets_1.py
@@ -4,4 +4,4 @@ lst = [random.randint(-10, 10) for _ in range(15)]
 
 print(lst)
 
-print('В этом списке', len(set(lst)), 'уникальных чисел.', sorted(set(lst)))
+print('В этом списке', len(set(lst)), 'уникальных чисел.')
