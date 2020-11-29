68e1c49ed4068a518fd97b958a6da9980b0f9a9b
CjVizer <cj_vizer@ukr.net>
2020-11-29 17:23:22 +0200

    HomeWork_Lesson_8

--- Lesson_8/HomeWork/Ledger.py
+++ Lesson_8/HomeWork/Ledger.py
@@ -1,7 +1,4 @@
-from pprint import pprint
-
 """
-
 1. Представьте себе бухгалтерскую книгу в книжном магазине. В этой книге хранятся
 записи о номере заказа, названии книги, колличестве и стоимости одной книги, как
 представленно ниже, в таблице.
@@ -23,7 +20,6 @@ from pprint import pprint
 и возвращает список кортежей. Каждый кортеж состоит из номера заказа и произведения
 цены на товары и количества. Стоимость товара должена быть увеличена на $10, если
 стоимость заказа меньше $100. Программа должна использовать lambda и map
-
 """
 
 ledger = [
@@ -33,20 +29,5 @@ ledger = [
     [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
 ]
 
-
-# pprint(ledger)
-
-
-# def func(a):
-#     for order in a:
-#         goods_sum = order[2] * order[3]
-#         if goods_sum < 100:
-#             goods_sum += 10
-#         print(order[0], order[2] * order[3], '"', round(goods_sum, 2), '"')
-
-
-res = tuple(map(lambda a: tuple(), ledger))
+res = list(map(lambda i: (i[0], round(i[2]*i[3], 2)) if i[2]*i[3] > 100 else (i[0], round(i[2]*i[3]+10, 2)), ledger))
 print(res)
-# func(ledger)
-# res = tuple()
-# print(res, type(res), len(ledger), ledger[0][0])
