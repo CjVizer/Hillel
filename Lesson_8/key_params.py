4251297377e2d58049d6c3ca513df91eeb6e4538
CjVizer <cj_vizer@ukr.net>
2020-11-29 11:56:27 +0200

    Lesson_8

---
+++ Lesson_8/key_params.py
@@ -0,0 +1,13 @@
+def pow(a=3, b=2):
+    print('a = ', a, 'b = ', b)
+    return a ** b
+
+
+res = pow(4, 5)
+print(res)
+
+res = pow(4, 2)
+print(res)
+
+res = pow(4)
+print(res)
---
+++ Lesson_8/lambda_func.py
@@ -0,0 +1,12 @@
+import random
+
+"""
+    lambda a, b, c: a + b + c
+"""
+
+func = lambda x, y: x + y
+
+print(func(3, 5))
+
+# map, filter, zip
+# map(func, collection)
