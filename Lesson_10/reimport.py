40e095e4e94f7a6b563bdc90fe2c2d0a1689b566
CjVizer <cj_vizer@ukr.net>
2020-12-10 12:25:15 +0200

    Lessons_10

---
+++ Lesson_10/reimport.py
@@ -0,0 +1,13 @@
+import importlib
+from time import sleep
+
+from Lesson_10.modules_m import mod3
+
+sleep(3)
+importlib.reload(mod3)
+sleep(3)
+importlib.reload(mod3)
+sleep(3)
+importlib.reload(mod3)
+sleep(3)
+importlib.reload(mod3)
