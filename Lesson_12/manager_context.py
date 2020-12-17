0d6aa2dd119b05f3a112565e48a85a58e40e6775
CjVizer <cj_vizer@ukr.net>
2020-12-17 08:46:20 +0200

    Lesson_12

---
+++ Lesson_12/manager_context.py
@@ -0,0 +1,17 @@
+with open('src.jpg', 'rb') as src, open('dst1.jpg', 'wb') as dst:
+    if src.closed is False:
+        print('File opened')
+    else:
+        print('File closed')
+
+    while True:
+        data = src.read(32)
+        if data:
+            dst.write(data)
+        else:
+            break
+
+if src.closed is False:
+    print('File opened')
+else:
+    print('File closed')
