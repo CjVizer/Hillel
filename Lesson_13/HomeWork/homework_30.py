6eea744496f67dde7fcfc4024da7f243d42b85ff
CjVizer <cj_vizer@ukr.net>
2020-12-21 10:55:43 +0200

    homework_30

--- Lesson_13/HomeWork/homework_30.py
+++ Lesson_13/HomeWork/homework_30.py
@@ -2,7 +2,7 @@
 По данному натуральному n вычислите сумму 1^3 + 2^3 + 3^3 + ... + n^3.
 """
 
-n = int(input('Please, enter a number: '))
+n = int(input('Пожалуйста, введите число: '))
 result = 0
 
 for i in range(1, n+1):
